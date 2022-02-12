88e5ea3423b77c27645a4905a41ee8a53bb6ce76
kimjungjin <jungks9351@gmail.com>
2022-02-13 08:07:11 +0900

    Feat firebase 비동기 통신을 위한 미들웨어 데이터 load 구현

--- src/redux/modules/words.js
+++ src/redux/modules/words.js
@@ -1,28 +1,48 @@
+import { db } from '../../firebase';
+import { addDoc, collection, getDoc, getDocs } from 'firebase/firestore';
+
 // 초기값 설정
 
 const initailState = {
-  list: [
-    {
-      word: 'ㅎ1ㅎ1',
-      desc: '히히를 변형한 단어.',
-      exam: '저 친구가 초콜릿을 줬어. ㅎ1ㅎ1',
-    },
-  ],
+  list: [],
 };
 
 // Action 타입 정의
-
+const LOAD = 'words/LOAD';
 const CREATE = 'words/CREATE';
 
 // Action 생성자 함수 생성
 // payload = word
+export const loadWord = (wordList) => {
+  return { type: LOAD, wordList };
+};
+
 export const createWord = (word) => {
   return { type: CREATE, word };
+};
+
+//middlewares
+
+// load
+export const loadWordFB = () => {
+  return async (dispatch) => {
+    const wordData = await getDocs(collection(db, 'words'));
+
+    let wordList = [];
+
+    wordData.forEach((doc) => {
+      wordList.push({ id: doc.id, ...doc.data() });
+    });
+    dispatch(loadWord(wordList));
+  };
 };
 
 // Reducer
 const reducer = (state = initailState, action = {}) => {
   switch (action.type) {
+    case LOAD: {
+      return { list: action.wordList };
+    }
     case CREATE: {
       const newWordList = [...state.list, action.word];
 
