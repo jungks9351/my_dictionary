e4a941afa202b887d2477e34d36608484adee2cc
kimjungjin <jungks9351@gmail.com>
2022-02-12 15:41:19 +0900

    Feat useRef를 사용하여 DOM요소 선택 구현

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import CommonHeader from '../components/common/CommonHeader';
 
 const WritePage = () => {
+  const inputWord = useRef(null);
+  const inputDesc = useRef(null);
+  const inputExam = useRef(null);
+
   return (
     <>
       <CommonHeader />
       <WritePageWrapper>
         <div>
           <h2>단어</h2>
-          <input type="text" id="word" />
+          <input type="text" id="word" ref={inputWord} />
         </div>
         <div>
           <h2>설명</h2>
-          <input type="text" id="word" />
+          <input type="text" id="desc" ref={inputDesc} />
         </div>
         <div>
           <h2>예시</h2>
-          <input type="text" id="word" />
+          <input type="text" id="exam" ref={inputExam} />
         </div>
         <button>추가하기</button>
       </WritePageWrapper>
