f3ff2958694e4b4167fe2b919d668893d23e90b0
kimjungjin <jungks9351@gmail.com>
2022-02-12 15:33:57 +0900

    Style 작성페이지 UI 구현

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
+import CommonHeader from '../components/common/CommonHeader';
 
 const WritePage = () => {
-  return <div>Write Page</div>;
+  return (
+    <>
+      <CommonHeader />
+      <WritePageWrapper>
+        <div>
+          <h2>단어</h2>
+          <input type="text" id="word" />
+        </div>
+        <div>
+          <h2>설명</h2>
+          <input type="text" id="word" />
+        </div>
+        <div>
+          <h2>예시</h2>
+          <input type="text" id="word" />
+        </div>
+        <button>추가하기</button>
+      </WritePageWrapper>
+    </>
+  );
 };
+
+const WritePageWrapper = styled.main`
+  width: 50vw;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+
+  input {
+    width: 100%;
+    background-color: #eee;
+  }
+
+  button {
+    margin: 0 auto;
+    width: 100px;
+    border: 1px solid #000;
+    padding: 1rem;
+  }
+`;
 
 export default WritePage;
