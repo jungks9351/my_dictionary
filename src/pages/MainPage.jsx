aa69b49386e816ef2befdae11c9d4976eeca2743
kimjungjin <jungks9351@gmail.com>
2022-02-12 15:31:37 +0900

    Style 메인페이지 UI 변경

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,22 +1,113 @@
 import React from 'react';
 import styled from 'styled-components';
 import CommonHeader from '../components/common/CommonHeader';
-import DictionaryList from '../components/main/DictionaryList';
 
 const MainPage = () => {
   return (
     <>
       <CommonHeader />
       <MainPageWrapper>
-        <DictionaryList />
+        <button>추가하기</button>
+        <ul>
+          <li>
+            <div className="word-box">
+              <h2>단어</h2>
+              <p>ㅎ1ㅎ1</p>
+            </div>
+            <div className="description-box">
+              <h2>설명</h2>
+              <p>
+                히히를 변형한 단어.
+                <br />
+                숫자 1을 "ㅣ"로 쓴다.
+              </p>
+            </div>
+            <div className="example-box">
+              <h2>예시</h2>
+              <p>저 친구가 초콜릿을 줬어. ㅎ1ㅎ1</p>
+            </div>
+          </li>
+          <li>
+            <div className="word-box">
+              <h2>단어</h2>
+              <p>ㅎ1ㅎ1</p>
+            </div>
+            <div className="description-box">
+              <h2>설명</h2>
+              <p>
+                히히를 변형한 단어.
+                <br />
+                숫자 1을 "ㅣ"로 쓴다.
+              </p>
+            </div>
+            <div className="example-box">
+              <h2>예시</h2>
+              <p>저 친구가 초콜릿을 줬어. ㅎ1ㅎ1</p>
+            </div>
+          </li>
+          <li>
+            <div className="word-box">
+              <h2>단어</h2>
+              <p>ㅎ1ㅎ1</p>
+            </div>
+            <div className="description-box">
+              <h2>설명</h2>
+              <p>
+                히히를 변형한 단어.
+                <br />
+                숫자 1을 "ㅣ"로 쓴다.
+              </p>
+            </div>
+            <div className="example-box">
+              <h2>예시</h2>
+              <p>저 친구가 초콜릿을 줬어. ㅎ1ㅎ1</p>
+            </div>
+          </li>
+        </ul>
       </MainPageWrapper>
     </>
   );
 };
 
-const MainPageWrapper = styled.main`
-  width: 100vw;
-  height: 60vh;
+const MainPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   margin: 0 auto;
+  button {
+    margin: 0 auto;
+    width: 100px;
+    border: 1px solid #000;
+    padding: 1rem;
+  }
+
+  ul {
+    margin: 0 auto;
+    display: flex;
+    gap: 2rem;
+    flex-wrap: wrap;
+  }
+
+  li {
+    width: 400px;
+    height: 400px;
+    box-shadow: 1px 5px 15px 1px rgba(0, 0, 0, 0.2);
+    padding: 3rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    h2 {
+      font-size: 2rem;
+      margin-bottom: 1rem;
+    }
+    p {
+      font-size: 1.8rem;
+    }
+    .example-box > p {
+      color: #0000ff;
+    }
+  }
 `;
+
 export default MainPage;
