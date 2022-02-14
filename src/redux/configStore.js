30d673a80d49717bf3874dae5e5469fdb1a87bd7
kimjungjin <jungks9351@gmail.com>
2022-02-14 18:35:11 +0900

    Feat redux-dev-tools 연결

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,12 +1,16 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import words from './modules/words';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middlewares = [thunk];
 const rootReducer = combineReducers({ words });
 
 const enhancer = applyMiddleware(...middlewares);
 
-const store = createStore(rootReducer, enhancer);
+const store = createStore(
+  rootReducer,
+  compose(enhancer, composeWithDevTools()),
+);
 
 export default store;
