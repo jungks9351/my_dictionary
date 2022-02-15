424c36a9a20b44ba38a618249031b1a519095b96
kimjungjin <jungks9351@gmail.com>
2022-02-16 01:26:21 +0900

    Feat CommonHeader 구현

--- src/components/common/CommonHeader.jsx
+++ src/components/common/CommonHeader.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const CommonHeader = () => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate('/');
+  };
   return (
     <HeaderWrapper>
-      <h1>나만의 사전</h1>
+      <h1 onClick={onClick}>My Dictionary</h1>
     </HeaderWrapper>
   );
 };
 
 const HeaderWrapper = styled.header`
-  width: 100vw;
+  width: 400px;
   height: 10vh;
+  background-color: #ace;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 4rem;
+  margin: 4rem auto;
   h1 {
-    font-size: 3rem;
+    font-size: 5rem;
+    font-weight: bolder;
+    cursor: pointer;
   }
 `;
 
